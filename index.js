10b624fa3d089bbd4c7a08a6168de74cc5a44eb5
Nicole Candiotti <nccandiotti@gmail.com>
2021-12-04 14:34:11 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(names) {
+    let messages = []
+    for (let i=0; i < names.length; i++) {
+        messages.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+function countDown(number) { 
+    let x = number
+        while (number >= 0) {
+            console.log(number--);
+            
+        }
+    }
+
