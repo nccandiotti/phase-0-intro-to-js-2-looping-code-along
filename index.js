a44e79a9740736fb7d3097b01b41ba2ec7e9db3e
Nicole Candiotti <nccandiotti@gmail.com>
2022-01-12 08:23:05 -0500

    commit tree 14d0bedf9e81ddeb0fc2b3c0708f5988fbe2fd2a

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
+// function writeCards(names) {
+//     let messages = []
+//     for (let i=0; i < names.length; i++) {
+//         messages.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+//     }
+//     return messages;
+// }
 
-function writeCards(names) {
-    let messages = []
-    for (let i=0; i < names.length; i++) {
-        messages.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
-    }
-    return messages;
-}
+// function countDown(number) {
+//     let x = number
+//         while (number >= 0) {
+//             console.log(number--);
 
-function countDown(number) { 
-    let x = number
-        while (number >= 0) {
-            console.log(number--);
-            
-        }
-    }
-
+//         }
+//     }
